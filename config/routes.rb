0a56502bf137114ef2dc113cd0442fb73b0f8ee1
neetmangat <neetmangat@gmail.com>
2020-07-17 07:29:46 +0000

    Added AJAX Post Request

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  resources :tasks
+  resources :tasks, only: [:index, :update, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
